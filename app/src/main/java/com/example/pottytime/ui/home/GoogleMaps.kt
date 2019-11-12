312b001f276c83801a07fbf9616e68cc47160000
pr0jectf3 <portillozachary@gmail.com>
2019-11-11 22:15:04 -0800

    added basic navigation map

---
+++ app/src/main/java/com/example/pottytime/ui/home/GoogleMaps.kt
@@ -0,0 +1,52 @@
+package com.example.pottytime.ui.home
+
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.databinding.DataBindingUtil
+import com.google.android.gms.maps.CameraUpdateFactory
+import com.google.android.gms.maps.GoogleMap
+import com.google.android.gms.maps.OnMapReadyCallback
+import com.google.android.gms.maps.SupportMapFragment
+import com.google.android.gms.maps.model.LatLng
+import com.google.android.gms.maps.model.MarkerOptions
+
+import com.example.pottytime.R
+import com.example.pottytime.databinding.FragmentGoogleMapsBinding
+
+/**
+ * A simple [Fragment] subclass.
+ */
+class GoogleMaps : Fragment(),OnMapReadyCallback {
+
+    private lateinit var mMap: GoogleMap
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+            // Inflate the layout for this fragment
+
+            val binding = DataBindingUtil.inflate<FragmentGoogleMapsBinding>(
+                    inflater,R.layout.fragment_google_maps,container,false)
+
+            val mapFragment = childFragmentManager.findFragmentById(R.id.mapView) as SupportMapFragment
+            mapFragment.getMapAsync(this)
+
+            return binding.root
+    }
+
+    override fun onMapReady(googleMap: GoogleMap) {
+        mMap = googleMap
+        val sydney = LatLng(-34.0,151.0)
+        mMap.addMarker(MarkerOptions().position(sydney).title("Marker in Sydney"))
+        mMap.moveCamera(CameraUpdateFactory.newLatLng(sydney))
+    }
+
+
+
+
+}
