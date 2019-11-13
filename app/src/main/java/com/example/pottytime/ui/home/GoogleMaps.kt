479448f4f382e0e3df56c94175d4745dbf7a3704
Zachary Portillo <portillozachary@gmail.com>
2019-11-12 20:39:00 -0800

    added points to map and took off action bar

--- app/src/main/java/com/example/pottytime/ui/home/GoogleMaps.kt
+++ app/src/main/java/com/example/pottytime/ui/home/GoogleMaps.kt
@@ -24,23 +24,29 @@ class GoogleMaps : Fragment(),OnMapReadyCallback {
 
     private lateinit var mMap: GoogleMap
 
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+    }
+
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
             // Inflate the layout for this fragment
 
-            val binding = DataBindingUtil.inflate<FragmentGoogleMapsBinding>(
-                    inflater,R.layout.fragment_google_maps,container,false)
+//            val binding = DataBindingUtil.inflate<FragmentGoogleMapsBinding>(
+//                    inflater,R.layout.fragment_google_maps,container,false)
+            var rootView = inflater.inflate(R.layout.fragment_google_maps, container, false)
 
             val mapFragment = childFragmentManager.findFragmentById(R.id.mapView) as SupportMapFragment
             mapFragment.getMapAsync(this)
 
-            return binding.root
+            return rootView
     }
 
-    override fun onMapReady(googleMap: GoogleMap) {
-        mMap = googleMap
+    override fun onMapReady(googleMap: GoogleMap?) {
+        mMap = googleMap!!
+        mMap.setMapType(GoogleMap.MAP_TYPE_NORMAL)
         val sydney = LatLng(-34.0,151.0)
         mMap.addMarker(MarkerOptions().position(sydney).title("Marker in Sydney"))
         mMap.moveCamera(CameraUpdateFactory.newLatLng(sydney))
--- app/src/main/java/com/example/pottytime/ui/home/HomeFragment.kt
+++ app/src/main/java/com/example/pottytime/ui/home/HomeFragment.kt
@@ -1,31 +1,65 @@
 package com.example.pottytime.ui.home
 
+import android.content.pm.PackageManager
+import android.location.Location
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.TextView
+import androidx.core.app.ActivityCompat
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProviders
 import com.example.pottytime.R
+import com.google.android.gms.maps.CameraUpdateFactory
+import com.google.android.gms.maps.GoogleMap
+import com.google.android.gms.maps.OnMapReadyCallback
+import com.google.android.gms.maps.SupportMapFragment
+import com.google.android.gms.maps.model.LatLng
+import com.google.android.gms.maps.model.MarkerOptions
 
-class HomeFragment : Fragment() {
+class HomeFragment : Fragment(), OnMapReadyCallback {
 
     private lateinit var homeViewModel: HomeViewModel
+    private lateinit var lastLocation: Location
+    private lateinit var mMap: GoogleMap
+
+    companion object {
+        private const val LOCATION_PERMISSION_REQUEST_CODE = 1
+    }
 
     override fun onCreateView(
-        inflater: LayoutInflater,
-        container: ViewGroup?,
+        inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        homeViewModel =
-            ViewModelProviders.of(this).get(HomeViewModel::class.java)
-        val root = inflater.inflate(R.layout.fragment_home, container, false)
-        val textView: TextView = root.findViewById(R.id.text_home)
-        homeViewModel.text.observe(this, Observer {
-            textView.text = it
-        })
-        return root
+        // Inflate the layout for this fragment
+
+//            val binding = DataBindingUtil.inflate<FragmentGoogleMapsBinding>(
+//                    inflater,R.layout.fragment_google_maps,container,false)
+        var rootView = inflater.inflate(R.layout.fragment_home, container, false)
+
+        val mapFragment = childFragmentManager.findFragmentById(R.id.mapView) as SupportMapFragment
+        mapFragment.getMapAsync(this)
+
+        return rootView
     }
+    override fun onMapReady(googleMap: GoogleMap?) {
+        mMap = googleMap!!
+        mMap.setMapType(GoogleMap.MAP_TYPE_TERRAIN)
+        val sydney = LatLng(40.73, -73.99)
+        mMap.addMarker(MarkerOptions().position(sydney).title("Marker in Sydney"))
+        mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(sydney,12.0f))
+        setUpMap()
+    }
+
+    private fun setUpMap() {
+        if (ActivityCompat.checkSelfPermission(this.requireContext(),
+                android.Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            ActivityCompat.requestPermissions(this.requireActivity(),
+                arrayOf(android.Manifest.permission.ACCESS_FINE_LOCATION), LOCATION_PERMISSION_REQUEST_CODE)
+            return
+        }
+    }
+
 }
