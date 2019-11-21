bf7796c4840376e30676909bd26f5bb1b9d3842d
Pasindu Siriwardena <pasindu2siri@gmail.com>
2019-11-20 19:35:28 -0800

    Finished layout of dashboard

--- app/src/main/java/com/example/pottytime/ui/dashboard/DashboardFragment.kt
+++ app/src/main/java/com/example/pottytime/ui/dashboard/DashboardFragment.kt
@@ -1,14 +1,22 @@
 package com.example.pottytime.ui.dashboard
 
+import android.content.Intent
 import android.os.Bundle
-import android.view.LayoutInflater
-import android.view.View
-import android.view.ViewGroup
+import android.view.*
+import android.widget.Button
+import android.widget.Switch
 import android.widget.TextView
+import androidx.core.app.NavUtils
+import androidx.databinding.DataBindingUtil
 import androidx.fragment.app.Fragment
+import androidx.fragment.app.FragmentTransaction
 import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProviders
+import androidx.navigation.Navigation
+import androidx.navigation.fragment.NavHostFragment
 import com.example.pottytime.R
+import kotlinx.android.synthetic.main.activity_main.*
+import kotlinx.android.synthetic.main.fragment_dashboard.view.*
 
 class DashboardFragment : Fragment() {
 
@@ -18,11 +26,26 @@ class DashboardFragment : Fragment() {
         inflater: LayoutInflater,
         container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View? {
-        dashboardViewModel =
-            ViewModelProviders.of(this).get(DashboardViewModel::class.java)
-        val root = inflater.inflate(R.layout.fragment_dashboard, container, false)
 
+    ): View?{
+        dashboardViewModel = ViewModelProviders.of(this).get(DashboardViewModel::class.java)
+        val root = inflater.inflate(R.layout.fragment_dashboard, container, false)
+        setHasOptionsMenu(true)
         return root
     }
+
+    override fun onCreateOptionsMenu(menu: Menu, inflater: MenuInflater) {
+        super.onCreateOptionsMenu(menu, inflater)
+        inflater.inflate(R.menu.edit, menu)
+        menu.findItem(R.id.edit).setVisible(true)
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean = when(item.itemId){
+       R.id.edit -> {
+            NavHostFragment.findNavController(nav_host_fragment).navigate(R.id.action_navigation_dashboard_to_editFragment)
+            true
+        }
+        else -> super.onOptionsItemSelected(item)
+    }
+
 }
---
+++ app/src/main/java/com/example/pottytime/ui/dashboard/EditFragment.kt
@@ -0,0 +1,40 @@
+package com.example.pottytime.ui.dashboard
+
+import android.os.Bundle
+import android.view.*
+import androidx.databinding.DataBindingUtil
+import androidx.fragment.app.Fragment
+import androidx.navigation.Navigation
+import androidx.navigation.fragment.NavHostFragment
+import com.example.pottytime.R
+import kotlinx.android.synthetic.main.activity_main.*
+import kotlinx.android.synthetic.main.fragment_edit.view.*
+
+class EditFragment : Fragment() {
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        val root = inflater.inflate(R.layout.fragment_edit, container, false)
+        setHasOptionsMenu(true)
+        return root
+    }
+
+
+    override fun onCreateOptionsMenu(menu: Menu, inflater: MenuInflater) {
+        super.onCreateOptionsMenu(menu, inflater)
+        inflater.inflate(R.menu.save, menu)
+        menu.findItem(R.id.save).setVisible(true)
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean = when(item.itemId){
+        R.id.save -> {
+            NavHostFragment.findNavController(nav_host_fragment).navigate(R.id.action_editFragment_to_navigation_dashboard)
+            true
+        }
+        else -> super.onOptionsItemSelected(item)
+    }
+
+}
