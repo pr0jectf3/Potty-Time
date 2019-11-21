d9b79acd8cc115581ffcfc045be8ab2d01eb22f9
Zachary Portillo <portillozachary@gmail.com>
2019-11-20 18:25:16 -0800

    added preferences and button xml

--- app/src/main/java/com/example/pottytime/ui/home/HomeFragment.kt
+++ app/src/main/java/com/example/pottytime/ui/home/HomeFragment.kt
@@ -9,6 +9,7 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.TextView
+import android.widget.Toast
 import androidx.core.app.ActivityCompat
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.Observer
@@ -23,6 +24,9 @@ import com.google.android.gms.maps.model.LatLngBounds
 import com.google.android.gms.maps.model.MarkerOptions
 import com.google.android.material.bottomsheet.BottomSheetDialog
 import com.google.firebase.firestore.FirebaseFirestore
+import kotlinx.android.synthetic.main.dialog_layout.*
+import kotlinx.android.synthetic.main.dialog_layout.view.*
+import kotlinx.android.synthetic.main.fragment_home.*
 import kotlinx.android.synthetic.main.fragment_home.view.*
 import kotlin.reflect.typeOf
 
@@ -31,6 +35,14 @@ class HomeFragment : Fragment(), OnMapReadyCallback {
     private lateinit var homeViewModel: HomeViewModel
     private lateinit var lastLocation: Location
     private lateinit var mMap: GoogleMap
+
+    private var isMen: Boolean = true
+    private var isNeutral: Boolean = true
+    private var isWomen: Boolean = true
+    private var isFamily: Boolean = true
+    private var isHandicap: Boolean = true
+    private var isSatellite: Boolean = true
+
     val db = FirebaseFirestore.getInstance()
 
     companion object {
@@ -50,18 +62,23 @@ class HomeFragment : Fragment(), OnMapReadyCallback {
         val mapFragment = childFragmentManager.findFragmentById(R.id.mapView) as SupportMapFragment
         mapFragment.getMapAsync(this)
 
+        //BOTTOM MODAL SHEET CODE
         rootView.homeButton.setOnClickListener {
             val dialog = BottomSheetDialog(this.requireContext())
             val view = layoutInflater.inflate(R.layout.dialog_layout,null)
+            setButtonPreferences(view)
             dialog.setContentView(view)
             dialog.show()
         }
+
+
+
 
         return rootView
     }
     override fun onMapReady(googleMap: GoogleMap?) {
         mMap = googleMap!!
-        mMap.setMapType(GoogleMap.MAP_TYPE_HYBRID)
+        if (isSatellite) mMap.setMapType(GoogleMap.MAP_TYPE_HYBRID) else mMap.setMapType(GoogleMap.MAP_TYPE_NORMAL)
         val schoolBounds = LatLngBounds(LatLng(34.061334,-118.174004),
             LatLng(34.071732,-118.162157)
         )
@@ -92,6 +109,35 @@ class HomeFragment : Fragment(), OnMapReadyCallback {
                 arrayOf(android.Manifest.permission.ACCESS_FINE_LOCATION), LOCATION_PERMISSION_REQUEST_CODE)
             return
         }
+    }
+
+    private fun setButtonPreferences(rootView: View){
+
+        rootView.menButton.setOnClickListener{
+            if(isMen) rootView.menButton.setImageResource(R.drawable.male_off) else rootView.menButton.setImageResource(R.drawable.male_on)
+            isMen = !isMen
+        }
+        rootView.womenButton.setOnClickListener{
+            if(isWomen) rootView.womenButton.setImageResource(R.drawable.female_off) else rootView.womenButton.setImageResource(R.drawable.female_on)
+            isWomen = !isWomen
+        }
+        rootView.familyButton.setOnClickListener{
+            if(isFamily) rootView.familyButton.setImageResource(R.drawable.family_off) else rootView.familyButton.setImageResource(R.drawable.family_on)
+            isFamily = !isFamily
+        }
+        rootView.handicapButton.setOnClickListener{
+            if(isHandicap) rootView.handicapButton.setImageResource(R.drawable.handicap_off) else rootView.handicapButton.setImageResource(R.drawable.handicap_on)
+            isHandicap = !isHandicap
+        }
+        rootView.neutralButton.setOnClickListener{
+            if(isNeutral) rootView.neutralButton.setImageResource(R.drawable.neutral_off) else rootView.neutralButton.setImageResource(R.drawable.neutral_on)
+            isNeutral = !isNeutral
+        }
+        rootView.mapSwitch.setOnCheckedChangeListener{buttonView, isChecked ->
+            if(isChecked) isSatellite = false else isSatellite = !isSatellite
+            onMapReady(mMap)
+        }
+
     }
 
 }
