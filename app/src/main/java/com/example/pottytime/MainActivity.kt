69f41e4fadfc49ce206b7350e4880a8a7ab6d3d3
chaninkou <michaelkoulove@yahoo.com>
2019-12-12 16:53:13 -0800

    Big update, used binding for dashboard, search page, main, also added nagivation

--- app/src/main/java/com/example/pottytime/MainActivity.kt
+++ app/src/main/java/com/example/pottytime/MainActivity.kt
@@ -3,6 +3,8 @@ package com.example.pottytime
 import android.os.Bundle
 import com.google.android.material.bottomnavigation.BottomNavigationView
 import androidx.appcompat.app.AppCompatActivity
+import androidx.databinding.DataBindingUtil
+import androidx.drawerlayout.widget.DrawerLayout
 import androidx.fragment.app.Fragment
 import com.example.pottytime.ui.dashboard.DashboardFragment
 import com.example.pottytime.ui.home.HomeFragment
@@ -10,17 +12,26 @@ import com.example.pottytime.ui.notifications.SearchUserFragment
 import com.example.pottytime.ui.bathroom.BathroomsFragment
 import com.google.firebase.auth.FirebaseAuth
 import kotlinx.android.synthetic.main.activity_main.*
-
+import androidx.navigation.findNavController
+import androidx.navigation.ui.NavigationUI
+import com.example.pottytime.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
     private var firebaseAuth: FirebaseAuth? = null
+    private lateinit var drawerLayout: DrawerLayout
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        //setContentView(R.layout.activity_main)
 //        val navView: BottomNavigationView = findViewById(R.id.nav_view)
-//
-//        val navController = findNavController(R.id.nav_host_fragment)
+        @Suppress("UNUSED_VARIABLE")
+        val binding = DataBindingUtil.setContentView<ActivityMainBinding>(this, R.layout.activity_main)
+
+        val navController = this.findNavController(R.id.nav_host_fragment)
+
+        // For drawer layout
+        NavigationUI.setupWithNavController(binding.bottomNavigation, navController)
+
 //        // Passing each menu ID as a set of Ids because each
 //        // menu should be considered as top level destinations.
 //        val appBarConfiguration = AppBarConfiguration(
@@ -41,40 +52,40 @@ class MainActivity : AppCompatActivity() {
 //        }
 
         // Have map fragment (homefragment) as default
-        replaceFragment(HomeFragment())
+        //replaceFragment(HomeFragment())
 
-        bottom_Navigation.setOnNavigationItemSelectedListener(mOnNavigationItemSelectedListner)
+        //bottom_Navigation.setOnNavigationItemSelectedListener(mOnNavigationItemSelectedListner)
     }
 
-    private val mOnNavigationItemSelectedListner = BottomNavigationView.OnNavigationItemSelectedListener { item ->
-        when(item.itemId){
-            R.id.navigation_map -> {
-                replaceFragment(HomeFragment())
-                return@OnNavigationItemSelectedListener true
-            }
-            R.id.navigation_dashboard -> {
-                replaceFragment(DashboardFragment())
-                return@OnNavigationItemSelectedListener true
-            }
-            R.id.navigation_notifications -> {
-                replaceFragment(SearchUserFragment())
-                return@OnNavigationItemSelectedListener true
-            }
-//            R.id.navigation_logout -> {
-//                replaceFragment(LogoutFragment())
+//    private val mOnNavigationItemSelectedListner = BottomNavigationView.OnNavigationItemSelectedListener { item ->
+//        when(item.itemId){
+//            R.id.navigation_map -> {
+//                replaceFragment(HomeFragment())
 //                return@OnNavigationItemSelectedListener true
 //            }
-            R.id.navigation_bathrooms -> {
-                replaceFragment(BathroomsFragment())
-                return@OnNavigationItemSelectedListener true
-            }
-        }
-        false
-    }
+//            R.id.navigation_dashboard -> {
+//                replaceFragment(DashboardFragment())
+//                return@OnNavigationItemSelectedListener true
+//            }
+//            R.id.navigation_notifications -> {
+//                replaceFragment(SearchUserFragment())
+//                return@OnNavigationItemSelectedListener true
+//            }
+////            R.id.navigation_logout -> {
+////                replaceFragment(LogoutFragment())
+////                return@OnNavigationItemSelectedListener true
+////            }
+//            R.id.navigation_bathrooms -> {
+//                replaceFragment(BathroomsFragment())
+//                return@OnNavigationItemSelectedListener true
+//            }
+//        }
+//        false
+//    }
 
-    private fun replaceFragment(fragment : Fragment){
-        val fragmentTransaction = supportFragmentManager.beginTransaction()
-        fragmentTransaction.replace(R.id.fragmentContainer, fragment)
-        fragmentTransaction.commit()
-    }
+//    private fun replaceFragment(fragment : Fragment){
+//        val fragmentTransaction = supportFragmentManager.beginTransaction()
+//        fragmentTransaction.replace(R.id.fragmentContainer, fragment)
+//        fragmentTransaction.commit()
+//    }
 }
--- app/src/main/java/com/example/pottytime/ui/dashboard/EditFragment.kt
+++ app/src/main/java/com/example/pottytime/ui/dashboard/EditFragment.kt
@@ -4,10 +4,13 @@ import android.content.Intent
 import android.os.Bundle
 import android.view.*
 import androidx.appcompat.app.AppCompatActivity
+import androidx.databinding.DataBindingUtil
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.FragmentTransaction
+import androidx.navigation.Navigation
 import com.example.pottytime.MainActivity
 import com.example.pottytime.R
+import com.example.pottytime.databinding.FragmentEditBinding
 import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.auth.FirebaseUser
 import com.google.firebase.firestore.FirebaseFirestore
@@ -28,10 +31,14 @@ class EditFragment : Fragment() {
         container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
+
         auth = FirebaseAuth.getInstance()
         currentUserUid = auth?.currentUser?.uid
 
-        val root = inflater.inflate(R.layout.fragment_edit, container, false)
+        val binding: FragmentEditBinding = DataBindingUtil.inflate(
+            inflater, R.layout.fragment_edit, container, false)
+
+//        val root = inflater.inflate(R.layout.fragment_edit, container, false)
         //val userEmail = FirebaseAuth.getInstance().currentUser!!.email ?: ""
         val user = db.collection("users").document(uid)
 
@@ -42,17 +49,17 @@ class EditFragment : Fragment() {
                 val gender = document.get("gender").toString()
                 val displayName = document.get("displayName").toString()
 
-                root.editFirstName.setText(firstName);
-                root.editLastName.setText(lastName);
-                root.editGender.setText(gender);
-                root.editDisplayName.setText(displayName);
+                binding.editFirstName.setText(firstName);
+                binding.editLastName.setText(lastName);
+                binding.editGender.setText(gender);
+                binding.editDisplayName.setText(displayName);
             }else {
                 println("No document")
             }
         }
 
         setHasOptionsMenu(true)
-        return root
+        return binding.root
     }
 
 
@@ -95,6 +102,7 @@ class EditFragment : Fragment() {
 
                     user.update(tempUser)
                     replaceFragment(DashboardFragment())
+                    //Navigation.createNavigateOnClickListener(R.id.action_editFragment_to_navigation_dashboard2)
                 }else {
                     println("No document")
                 }
@@ -109,8 +117,17 @@ class EditFragment : Fragment() {
 
 
     private fun replaceFragment(fragment: Fragment) {
-        val fragmentTransaction = (activity as AppCompatActivity).supportFragmentManager.beginTransaction()
-        fragmentTransaction.replace(R.id.fragmentContainer, fragment)
-        fragmentTransaction.commit()
+//        val fragmentTransaction = (activity as AppCompatActivity).supportFragmentManager.beginTransaction()
+          //Navigation.createNavigateOnClickListener(R.id.action_editFragment2_to_navigation_dashboard)
+//        fragmentTransaction.replace(R.id.nav_host_fragment, fragment)
+//        //fragmentTransaction.addToBackStack(null);
+//        fragmentTransaction.commit()
+        val manager = fragmentManager
+
+        val frag_trans = manager!!.beginTransaction()
+
+        frag_trans!!.replace(R.id.nav_host_fragment, fragment)
+        frag_trans.addToBackStack(null);
+        frag_trans!!.commit()
     }
 }
